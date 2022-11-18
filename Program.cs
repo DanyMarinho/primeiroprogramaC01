94669a5c12289ffd2d1d32f978b870b790702fe1
DanyMarinho <cardosodanielly11@gmail.com>
2022-11-18 11:07:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿const string textoolamundo = "Olá mundo, esse é o meu primeiro programa C# feito por";
+string nomecompleto;
+int idade;
+Console.WriteLine("Qual o seu nome completo?");
+nomecompleto = Console.ReadLine().ToString();
+Console.WriteLine("Qual a sua idade");
+idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(textoolamundo + nomecompleto + "que tem" + idade + "anos");
